ca231d1a04e199efb87d4cc9c4990c5f3f1d3811
Sandeepshinde630 <sandeepshinde630@gmail.com>
2021-09-21 16:48:55 +0530

    first commit

---
+++ AddressBook_Java.js
@@ -0,0 +1,167 @@
+//UC-1
+class AddressBook {
+
+    constructor(...params) {
+    this.firstName = params[0];
+    this.lastName = params[1];
+    this.address = params[2];
+    this.city = params[3];
+    this.state = params[4];
+    this.zipCode = params[5];
+    this.phoneNumber = params[6];
+    this.emailId = params[7];
+}
+//UC-2
+    get firstName() { return this._firstName; }
+    set firstName(firstName) {
+    let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$"); 
+    if (nameRegex.test(firstName))
+    this._firstName = firstName;
+    else
+    throw "Invalid first Name";
+    }
+
+    get lastName() { return this._lastName; }
+    set lastName(lastName) {
+    let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+    if (nameRegex.test(lastName))
+    this._lastName = lastName;
+        else
+        throw "Invalid last Name";
+    }    
+
+    get address() { return this._address; }
+    set address(address) {
+    let addressRegex = RegExp('^[0-9A-Z]{1}[A-Z a-z 0-9]{1,}$'); 
+    if (addressRegex.test(address))
+    this._address = address;
+        else
+        throw "Invalid address ";
+    }
+        
+    get city() { return this._city; }
+    set city(city) {
+    let cityRegex = RegExp("^[A-Za-z]{4,}$"); 
+    if (cityRegex.test(city))
+    this._city = city;
+        else
+        throw "Invalid city ";
+    }
+
+    get state() { return this._state; }
+    set state(state) {
+    let stateRegex = RegExp("^[A-Za-z0-9]{4,}$");
+    if (stateRegex.test(state))
+    this._state = state;
+    else
+        throw "Invalid state";
+    }
+    
+    get zipCode() { return this._zipCode; }
+    set zipCode(zipCode) {
+    let zipRegex = RegExp("^[1-9]{1}[0-9]{5}$"); 
+    if (zipRegex.test(zipCode))
+    this._zipCode = zipCode;
+        else
+        throw "Invalid zipCode";
+    }
+    
+    get phoneNumber() { return this._phoneNumber; }
+    set phoneNumber(phoneNumber) {
+    let phoneRegex = RegExp("^[1-9]{2}[ ]{1}[0-9]{10}$"); 
+    if (phoneRegex.test(phoneNumber))
+    this._phoneNumber = phoneNumber;
+        else
+        throw "Invalid phoneNumber";
+    }
+    
+    get emailId() { return this._emailId; }
+    set emailId(emailId) {                        
+    let emailRegex = RegExp("^([a-z0-9A-Z])+([.]?[a-z0-9A-Z]+)*[@]{1}[a-z0-9A-Z]+[.]{1}[a-zA-Z]{2,}([.]{1}[a-z]{2,})?$");
+    if (emailRegex.test(emailId))
+    this._emailId = emailId;
+        else
+    throw "Invalid email";
+    }
+
+    toString() {
+    return "first Name: " + this.firstName + ", last Name: " + this.lastName+ "\nAddress: " + this.address + ", \nCity: " + this.city
+    + "\nState: " + this.state + "\nZipCode: " + this.zipCode + ", \nPhoneNumber: " + this.phoneNumber + ", \nemailId: " + this.emailId +"\n";   
+     }
+    }
+        //UC-3
+    let details = new Array();
+    try {
+        details.push(new AddressBook("Sandeep","Sinde","2319  Padoli","Chandrapur","Maharashtra","442406","91 9999999999","sandeep223@gmail.com"));
+        details.push(new AddressBook("Nikhil","Raut","2846  Ramnagar","Nagpur","Maharashtra","234567","91 8888888888","nikhil243@ymail.net"));    
+}
+catch(e)
+{
+    console.error(e);
+}
+Display();
+function Display(){
+    details.forEach((contact) => console.log(contact.toString()));
+}
+
+//UC-4
+let index = details.findIndex(contact=>contact.firstName == 'Sandeep');
+details[index].lastName = 'Shinde';
+console.log("++++++++++++++++++++++++++++++++++++++")
+console.log("Edited Address Book Successfully: ");
+Display();
+
+//UC-5
+details.splice(index, 1);
+console.log("++++++++++++++++++++++++++++++++++++++")
+console.log("AddressBook after Contact deleted");
+Display();
+
+//UC-6
+let contactDetails=0;
+function getCount(details){
+    if (details != null)
+        contactDetails++;
+    return contactDetails;
+}
+details.reduce(getCount,1);
+console.log("++++++++++++++++++++++++++++++++++++++++");
+console.log("Total Contacts : " + contactDetails);
+
+//UC-7
+let personContact = new AddressBook("Nikhil","Raut","2846  Ramnagar","Nagpur","Maharashtra","234567","91 8888888888","nikhil243@ymail.net");
+if(details.some(e => e._lastName == "Raut"))
+    console.log("Contact already Exists!");
+else{
+    details.push(personContact);
+    console.log("Contact added succsefully");
+}
+console.log("Array: ",details);
+
+//UC-8
+let findByCity = details.filter((e) => e._city == 'Nagpur');
+console.log("Contacts by city: ",findByCity);
+let findByState = details.filter((e) => e._state == 'Maharashtra');
+console.log("Contacts by state: ",findByState);
+
+
+//UC-9
+console.log(details.filter(contact => contact._city == "Nagpur")
+        .map(contact => contact.firstName));
+console.log(details.filter(contact => contact._state == "Maharashtra")
+        .map(contact => contact._firstName));
+//UC-10
+console.log(details.filter(contact => contact.city == "Nagpur")
+        .reduce((count, contact) => contact.firstName ? ++count : count,0));
+console.log(details.filter(contact => contact.state == "Maharashtra")
+            .reduce((count, contact) => contact.firstName ? ++count : count, 0)); 
+
+//UC-11
+console.log(details.sort((a, b) => a.lastName.localeCompare(b.lastName)));
+//UC-12
+console.log("Contact After Sorting City:-");
+console.log(details.sort((a, b) => a.city.localeCompare(b.city)))
+console.log("Contact After sorting State:-");
+console.log(details.sort((a, b) => a.state.localeCompare(b.state)));
+console.log("Contact After sorting zip:-");
+console.log(details.sort((a, b) => a.zipCode.localeCompare(b.zipCode)));
